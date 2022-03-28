aae719db31542fb2837b5b287c76cce59f8fec65
hadiepsixiang <hadiep2023@gmail.com>
2022-03-28 17:38:05 +0700

    ss

---
+++ src/Components/chart_act/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import "./style.css"
+
+export default function chart_act() {
+  return (
+    <div>chart</div>
+  )
+}
---
+++ src/Components/listtask/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./style.css"
+
+export default function listtask() {
+    return (
+        <div className="list-task">
+            <div class="listtext">
+                <h2 className="db-subtext">Danh sách công việc cần làm</h2>
+                <div className="jobtask">
+                    <div className="jobtask_01">
+                        <div className="listnum">23</div>
+                        <span>Đơn chờ xác nhận</span>
+                    </div>
+                    <div className="jobtask_01">
+                        <div className="listnum">43</div>
+                        <span>Đơn chờ lấy hàng</span>
+                    </div>
+                    <div className="jobtask_01">
+                        <div className="listnum">06</div>
+                        <span>Đơn hủy</span>
+                    </div>
+                    <div className="jobtask_01">
+                        <div className="listnum">03</div>
+                        <span>Yêu cầu hoàn trả</span>
+                    </div>
+                    <div className="jobtask_01">
+                        <div className="listnum">10</div>
+                        <span>Sản phẩm hết hàng</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
