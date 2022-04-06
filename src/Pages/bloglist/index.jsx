62cbd3269343058f7969012490dcfc15f076e32f
hadiepsixiang <hadiep2023@gmail.com>
2022-04-07 00:16:53 +0700

    updated

--- src/Pages/bloglist/index.jsx
+++ src/Pages/bloglist/index.jsx
@@ -3,44 +3,6 @@ import "./style.css"
 import { DataGrid } from '@mui/x-data-grid';
 import { Link } from 'react-router-dom'
 
-const columns = [
-    { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'title', headerName: 'Tiêu đề', width: 260 },
-    { field: 'cate', headerName: 'Danh mục', width: 260 },
-    {
-        field: 'time',
-        headerName: 'Thời gian đăng',
-        width: 160,
-        type: 'datetime',
-    },
-    {
-        field: 'action',
-        headerName: 'Thao tác',
-        width: 160,
-        renderCell: (params) => {
-            return (
-                <div >
-                    <img className="userEdit" src="././assets/images/edit.svg" />
-                    <img className="userDel" src="././assets/images/trash-2.svg" />
-                </div>
-            )
-        }
-    }
-];
-const row = [
-    { id: 1, title: 'Cách trang trí nội thất theo hướng tối giản', cate: 'Trang trí nội thất', time: '28/03/2022' },
-    { id: 2, title: 'Cách dán màn hình điện thoại đơn giản', cate: 'Phụ kiện điện thoại', time: '25/03/2022' },
-    { id: 3, title: 'Cách phối đồ với phụ kiện sang trọng', cate: 'Phụ kiện thời trang', time: '23/03/2022' },
-    { id: 4, title: 'Cách chọn mua đồ dùng học tập', cate: 'Văn phòng phẩm', time: '22/03/2022' },
-    { id: 5, title: 'Ý nghĩa của quà tặng vào những ngày đặc biệt', cate: 'Quà tặng', time: '01/03/2022' },
-    { id: 6, title: 'Cách trang trí phòng ngủ ấm cúng, đơn giản', cate: 'Trang trí nội thất', time: '21/03/2022' },
-    { id: 7, title: 'Hướng dẫn đo size nhẫn chuẩn nhất', cate: 'Phụ kiện thời trang', time: '14/02/2022' },
-    { id: 8, title: 'Ý nghĩa việc tặng quà cho người thân', cate: 'Quà tặng', time: '11/02/2022' },
-    { id: 9, title: 'Cách chọn bàn học làm phù hợp cho phòng', cate: 'Trang trí nội thất', time: '18/01/2022' },
-    { id: 10, title: 'Mách bạn cách chọn đèn ngủ đẹp lung linh', cate: 'Trang trí nội thất', time: '05/01/2022' },
-    { id: 11, title: 'Ý tưởng trang trí phòng ngủ đẹp dễ thương', cate: 'Trang trí nội thất', time: '15/12/2021' },
-    { id: 12, title: 'Trang trí phòng ngủ theo phong cách vintage', cate: 'Trang trí nội thất', time: '01/12/2021' },
-];
 export default function bloglist() {
     return (
         <div className="Blogs-page">
@@ -54,14 +16,170 @@ export default function bloglist() {
                 </Link>
             </div>
             <div className="blogform">
-                <div style={{ height: 550, width: '100%' }}>
-                    <DataGrid
-                        rows={row} disableSelectionOnClick
-                        columns={columns}
-                        pageSize={8}
-                        rowsPerPageOptions={[5]}
-                        checkboxSelection
-                    />
+                <div className="widgetLg">
+                    <table className="widgetLgTable">
+                        <tr className="widgetLgTr">
+                            <th className="widgetLgTh">Tiêu đề</th>
+                            <th className="widgetLgTh">Danh mục</th>
+                            <th className="widgetLgTh">Phân loại</th>
+                            <th className="widgetLgTh">Thao tác</th>
+
+                        </tr>
+                        <tr className="widgetLgTr">
+                            <td className="widgetLgName">Cách trang trí nội thất theo hướng tối giản</td>
+                            <td className="widgetLgName">Trang trí nội thất</td>
+                            <td className="widgetLgCate">6/3/2022</td>
+                            <td className="widgetLgAct">
+                                <img
+                                    src="././assets/images/edit.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                                <img
+                                    src="././assets/images/trash-2.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                            </td>
+                        </tr>
+                        <tr className="widgetLgTr">
+                            <td className="widgetLgName">Cách dán màn hình điện thoại đơn giản</td>
+                            <td className="widgetLgName">Phụ kiện điện thoại</td>
+                            <td className="widgetLgCate">5/3/2022</td>
+                            <td className="widgetLgAct">
+                                <img
+                                    src="././assets/images/edit.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                                <img
+                                    src="././assets/images/trash-2.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                            </td>
+                        </tr>
+                        <tr className="widgetLgTr">
+                            <td className="widgetLgName">Cách phối đồ với phụ kiện sang trọng</td>
+                            <td className="widgetLgName">Phụ kiện thời trang</td>
+                            <td className="widgetLgCate">4/3/2022</td>
+                            <td className="widgetLgAct">
+                                <img
+                                    src="././assets/images/edit.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                                <img
+                                    src="././assets/images/trash-2.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                            </td>
+                        </tr>
+                        <tr className="widgetLgTr">
+                            <td className="widgetLgName">Cách chọn mua đồ dùng học tập</td>
+                            <td className="widgetLgName">Văn phòng phẩm</td>
+                            <td className="widgetLgCate">3/3/2022</td>
+                            <td className="widgetLgAct">
+                                <img
+                                    src="././assets/images/edit.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                                <img
+                                    src="././assets/images/trash-2.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                            </td>
+                        </tr>
+                        <tr className="widgetLgTr">
+                            <td className="widgetLgName">Ý nghĩa của quà tặng vào những ngày đặc biệt</td>
+                            <td className="widgetLgName">Quà tặng</td>
+                            <td className="widgetLgCate">2/3/2022</td>
+                            <td className="widgetLgAct">
+                                <img
+                                    src="././assets/images/edit.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                                <img
+                                    src="././assets/images/trash-2.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                            </td>
+                        </tr>
+                        <tr className="widgetLgTr">
+                            <td className="widgetLgName">Chọn nhẫn theo cung hoàng đạo </td>
+                            <td className="widgetLgName">Phụ kiện thời trang </td>
+                            <td className="widgetLgCate">1/3/2022</td>
+                            <td className="widgetLgAct">
+                                <img
+                                    src="././assets/images/edit.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                                <img
+                                    src="././assets/images/trash-2.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                            </td>
+                        </tr>
+                        <tr className="widgetLgTr">
+                            <td className="widgetLgName">Cách chọn mua đồ dùng học tập</td>
+                            <td className="widgetLgName">Văn phòng phẩm</td>
+                            <td className="widgetLgCate">3/3/2022</td>
+                            <td className="widgetLgAct">
+                                <img
+                                    src="././assets/images/edit.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                                <img
+                                    src="././assets/images/trash-2.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                            </td>
+                        </tr>
+                        <tr className="widgetLgTr">
+                            <td className="widgetLgName">Ý nghĩa của quà tặng vào những ngày đặc biệt</td>
+                            <td className="widgetLgName">Quà tặng</td>
+                            <td className="widgetLgCate">2/3/2022</td>
+                            <td className="widgetLgAct">
+                                <img
+                                    src="././assets/images/edit.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                                <img
+                                    src="././assets/images/trash-2.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                            </td>
+                        </tr>
+                        <tr className="widgetLgTr">
+                            <td className="widgetLgName">Chọn nhẫn theo cung hoàng đạo </td>
+                            <td className="widgetLgName">Phụ kiện thời trang </td>
+                            <td className="widgetLgCate">1/3/2022</td>
+                            <td className="widgetLgAct">
+                                <img
+                                    src="././assets/images/edit.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                                <img
+                                    src="././assets/images/trash-2.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                            </td>
+                        </tr>
+                        
+                    </table>
                 </div>
             </div>
         </div>
--- src/Pages/userData/index.jsx
+++ src/Pages/userData/index.jsx
@@ -4,63 +4,191 @@ import { DataGrid } from '@mui/x-data-grid';
 
 
 
-const columns = [
-    { field: 'id', headerName: 'ID', width: 90 },
-    { field: 'username', headerName: 'Tên đăng nhập', width: 200 },
-    { field: 'email', headerName: 'Email', width: 260 },
-    {
-        field: 'status',
-        headerName: 'Trạng thái',
-        width: 160,
-        renderCell: (params) => {
-            return (
-                <div className="userSt">
-                    {params.row.status}
-                </div>
-            )
-        }
-    },
-    {
-        field: 'action',
-        headerName: 'Thao tác',
-        width: 160,
-        renderCell: (params) => {
-            return (
-                <div >
-                    <img className="userEdit" src="././assets/images/edit.svg" />
-                    <img className="userDel" src="././assets/images/trash-2.svg" />
-                </div>
-            )
-        }
-    }
-];
-const row = [
-    { id: 1, username: 'admin01', email: 'dungvu1404@gmail.com', status: 'admin' },
-    { id: 2, username: 'admin02', email: 'nhatdiepchithu@gmail.com', status: 'admin' },
-    { id: 3, username: 'huongnguyen2003', email: 'huong2003@gmail.com', status: 'user' },
-    { id: 4, username: 'vannt2000', email: 'vannt2000@gmail.com', status: 'user' },
-    { id: 5, username: 'admin03', email: 'lananh123@gmail.com', status: 'admin' },
-    { id: 6, username: 'lanng678', email: 'lanng678@gmail.com', status: 'user' },
-    { id: 7, username: 'hoalan120', email: 'hoalan120@gmail.com', status: 'user' },
-    { id: 8, username: 'vyanh123', email: 'vyanh890@gmail.com', status: 'user' },
-    { id: 9, username: 'huongnguyen2003', email: 'huongnguyen2003@gmail.com', status: 'user' },
-    { id: 10, username: 'vannt2000', email: 'vannt2000@gmail.com', status: 'user' },
-    { id: 11, username: 'lanng678', email: 'lanng678@gmail.com', status: 'user' },
-    { id: 12, username: 'hoalan120', email: 'hoalan120@gmail.com', status: 'user' },
-    { id: 13, username: 'vyanh123', email: 'vyanh123@gmail.com', status: 'user' },
-  ];
 export default function userData() {
     return (
         <div className="userList">
-            <div style={{ height: 550, width: '100%' }}>
-                <DataGrid
-                    rows={row} disableSelectionOnClick 
-                    columns={columns}
-                    pageSize={8}
-                    rowsPerPageOptions={[5]}
-                    checkboxSelection
-                />
-            </div>
+            <div className="widgetLg">
+                    <table className="widgetLgTable">
+                        <tr className="widgetLgTr">
+                            <th className="widgetLgTh">Tên đăng nhập</th>
+                            <th className="widgetLgTh">Email</th>
+                            <th className="widgetLgTh">Trạng thái</th>
+                            <th className="widgetLgTh">Thao tác</th>
+
+                        </tr>
+                        <tr className="widgetLgTr">
+                            <td className="widgetLgName">dungvu1404</td>
+                            <td className="widgetLgName">dungvu1404@gmail.com</td>
+                            <td className="widgetLgCate1">Admin</td>
+                            <td className="widgetLgAct">
+                                <img
+                                    src="././assets/images/edit.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                                <img
+                                    src="././assets/images/trash-2.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                            </td>
+                        </tr>
+                        <tr className="widgetLgTr">
+                            <td className="widgetLgName">nhatdiepchithu</td>
+                            <td className="widgetLgName">nhatdiepchithu@gmail.com</td>
+                            <td className="widgetLgCate1">Admin</td>
+                            <td className="widgetLgAct">
+                                <img
+                                    src="././assets/images/edit.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                                <img
+                                    src="././assets/images/trash-2.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                            </td>
+                        </tr>
+                        <tr className="widgetLgTr">
+                            <td className="widgetLgName">huongnguyen2003</td>
+                            <td className="widgetLgName">huong2003@gmail.com</td>
+                            <td className="widgetLgCate">User</td>
+                            <td className="widgetLgAct">
+                                <img
+                                    src="././assets/images/edit.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                                <img
+                                    src="././assets/images/trash-2.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                            </td>
+                        </tr>
+                        <tr className="widgetLgTr">
+                            <td className="widgetLgName">vannt2000</td>
+                            <td className="widgetLgName">vannt20@gmail.com</td>
+                            <td className="widgetLgCate">User</td>
+                            <td className="widgetLgAct">
+                                <img
+                                    src="././assets/images/edit.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                                <img
+                                    src="././assets/images/trash-2.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                            </td>
+                        </tr>
+                        <tr className="widgetLgTr">
+                            <td className="widgetLgName">lananh123</td>
+                            <td className="widgetLgName">lananh890@gmail.com</td>
+                            <td className="widgetLgCate1">Admin</td>
+                            <td className="widgetLgAct">
+                                <img
+                                    src="././assets/images/edit.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                                <img
+                                    src="././assets/images/trash-2.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                            </td>
+                        </tr>
+                        <tr className="widgetLgTr">
+                            <td className="widgetLgName">lanng678</td>
+                            <td className="widgetLgName">lanng@gmail.com</td>
+                            <td className="widgetLgCate">User</td>
+                            <td className="widgetLgAct">
+                                <img
+                                    src="././assets/images/edit.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                                <img
+                                    src="././assets/images/trash-2.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                            </td>
+                        </tr>
+                        <tr className="widgetLgTr">
+                            <td className="widgetLgName">hoalan120</td>
+                            <td className="widgetLgName">hoalan120@gmail.com</td>
+                            <td className="widgetLgCate">User</td>
+                            <td className="widgetLgAct">
+                                <img
+                                    src="././assets/images/edit.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                                <img
+                                    src="././assets/images/trash-2.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                            </td>
+                        </tr>
+                        <tr className="widgetLgTr">
+                            <td className="widgetLgName">vyanh123</td>
+                            <td className="widgetLgName">vyanh890@gmail.com</td>
+                            <td className="widgetLgCate">User</td>
+                            <td className="widgetLgAct">
+                                <img
+                                    src="././assets/images/edit.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                                <img
+                                    src="././assets/images/trash-2.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                            </td>
+                        </tr>
+                        <tr className="widgetLgTr">
+                            <td className="widgetLgName">lananh123</td>
+                            <td className="widgetLgName">lananh890@gmail.com</td>
+                            <td className="widgetLgCate">User</td>
+                            <td className="widgetLgAct">
+                                <img
+                                    src="././assets/images/edit.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                                <img
+                                    src="././assets/images/trash-2.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                            </td>
+                        </tr>
+                        <tr className="widgetLgTr">
+                            <td className="widgetLgName">vannt2000</td>
+                            <td className="widgetLgName">vannt20@gmail.com</td>
+                            <td className="widgetLgCate">User</td>
+                            <td className="widgetLgAct">
+                                <img
+                                    src="././assets/images/edit.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                                <img
+                                    src="././assets/images/trash-2.svg"
+                                    alt=""
+                                    className="widgetLgActimg"
+                                />
+                            </td>
+                        </tr>
+                        
+                    </table>
+                </div>
         </div>
     )
 }
